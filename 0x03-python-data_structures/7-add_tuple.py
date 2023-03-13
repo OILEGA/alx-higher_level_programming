b81ac6a08eaf47fa93d67c3d9ab3451c566bb68b
OILEGA <get2mena@gmail.com>
2023-03-13 16:57:47 +0100

    a function that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -15,5 +15,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif lenB < 2:
         tuple_b = (tuple_b[0], 0)
 
-    res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return res
+    sum_turple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return sum_turple
