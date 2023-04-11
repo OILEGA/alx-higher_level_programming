6170237e7c130f9a27ecd820409ea63aa8d9cc6f
OILEGA <get2mena@gmail.com>
2023-04-11 21:42:55 +0100

    inheritance in python

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This part would contain the lookup function
+"""
+
+
+def lookup(obj):
+    """The return function returns a list of all the attributes and 
+       methods of the object including built-in attributes and methods."""
+    return dir(obj)
