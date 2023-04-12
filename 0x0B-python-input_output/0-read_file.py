1d237d8f178d852d7cfe00841d630764b9189f0c
OILEGA <get2mena@gmail.com>
2023-04-12 23:09:49 +0100

    python input/output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-contains the read_file function
+Contains the read_file function
 """
 
 
 def read_file(filename=""):
-	"""reads a text file and then print it to stdout"""
-	with open('filename', 'r', encoding="utf-8") as f:
-		print(f.read(), end="")
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
+
