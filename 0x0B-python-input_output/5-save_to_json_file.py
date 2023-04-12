474e1912f0598cc6ee8935fdbe1202a76cbff74c
OILEGA <get2mena@gmail.com>
2023-04-12 23:59:53 +0100

    python input/output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-contains the json str function
+function that writes an Object to a text file
 """
 
 import json
 
 
-def from_json_string(my_str):
-    """returns an object represented by a JSON string"""
-    return json.loads(my_str)
+def save_to_json_file(my_obj, filename):
+    """Object to a text file, using a JSON representation"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
+
