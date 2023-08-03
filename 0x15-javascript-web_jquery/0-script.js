3419b0ec211dcb941174a8e4068f4420075a4647
OILEGA <117779858+OILEGA@users.noreply.github.com>
2023-08-03 20:00:06 +0100

    a JavaScript script that updates the text color of the <header> element to red (#FF0000)

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,1 @@
-#!/usr/bin/node
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
-});
+document.querySelector('HEADER').style.color = '#FF0000';
